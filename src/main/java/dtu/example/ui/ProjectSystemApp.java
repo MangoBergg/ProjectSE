c1d8aac733e189c656d3198c6b219f1dcdb50a3d
MangoBergg <s224763@student.dtu.dk>
2024-04-22 16:08:41 +0200

    Can create project from the systemApp

---
+++ src/main/java/dtu/example/ui/ProjectSystemApp.java
@@ -0,0 +1,13 @@
+package dtu.example.ui;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProjectSystemApp {
+
+    public List<Project> projectList;
+
+    public ProjectSystemApp() {
+        projectList = new ArrayList<>();
+    }
+}
--- src/test/java/example/cucumber/CreateProject.java
+++ src/test/java/example/cucumber/CreateProject.java
@@ -1,27 +1,36 @@
 package example.cucumber;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import dtu.example.ui.ProjectSystemApp;
+import dtu.example.ui.Project;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CreateProject {
+	private ProjectSystemApp projectSystemApp;
+	private Project testProject;
+
+	public CreateProject() {
+		projectSystemApp = new ProjectSystemApp();
+	}
+
 	@Given("the employee attempts to create a new project")
 	public void the_employee_attempts_to_create_a_new_project() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		assertTrue(true);
 	}
 
 	@When("there is no existing project")
 	public void there_is_no_existing_project() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		assertFalse(projectSystemApp.projectList.contains(testProject));
 	}
 
 	@Then("the project is successfully added to the system")
 	public void the_project_is_successfully_added_to_the_system() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		projectSystemApp.projectList.add(testProject);
+		assertTrue(projectSystemApp.projectList.contains(testProject));
 	}
 }
