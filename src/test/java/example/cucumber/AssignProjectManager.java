eb73a21faffa5a07e48dc37e05b634e42366b869
s194242 <s194242@student.dtu.dk>
2024-05-01 17:35:30 +0200

    et j er rettet til h

--- src/test/java/example/cucumber/AssignProjectManager.java
+++ src/test/java/example/cucumber/AssignProjectManager.java
@@ -26,11 +26,11 @@ public class AssignProjectManager {
     @When("the user registers a project manager")
     public void theUserRegistersAProjectManager() throws Exception {
         testProject = projectManagementApp.getProjectFromName("Project");
-        testProject.setProjectManager(new Employee("juba"));
+        testProject.setProjectManager(new Employee("huba"));
     }
 
     @Then("the project manager is updated")
     public void theProjectManagerIsUpdated() {
-        assertTrue(testProject.getProjectManager().getEmployeeID().equals("juba"));
+        assertTrue(testProject.getProjectManager().getEmployeeID().equals("huba"));
     }
 }
