ec3d8669d34ee9a367145f219e816b3d70f019f2
Frederik Winther Bæk <frederik.baek@hotmail.com>
2024-05-04 13:59:41 +0200

    Update ProjectManagementApp.java

--- src/main/java/dtu/example/ui/ProjectManagementApp.java
+++ src/main/java/dtu/example/ui/ProjectManagementApp.java
@@ -49,7 +49,7 @@ public class ProjectManagementApp {
         activity.setParentProject(project);
 
         //Post-conditions
-        assert(project.getActivityList().contains(activity)) : "The activity now exists in activityList";
+        assert(project.getActivityList().contains(activity)) : "The activity now exists in activityList, which means it is created";
 
         return activity;
     }
