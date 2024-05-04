aae1acec57437495b964554c2ec892e9044c1c9f
s194242 <s194242@student.dtu.dk>
2024-05-04 14:44:03 +0200

    assert i update start end weeks for

--- src/test/java/example/TDD_BDD/ChangeStartAndEndWeeks.java
+++ src/test/java/example/TDD_BDD/ChangeStartAndEndWeeks.java
@@ -38,7 +38,7 @@ public class ChangeStartAndEndWeeks {
 
     @Then("the start and end weeks of the project containing the activity are updated to {int} and {int}")
     public void the_start_and_end_weeks_of_the_project_containing_the_activity_are_updated_to_and(Integer int1, Integer int2) {
-        assertTrue(testActivity.getParentProject().getStartEndWeeks()[0] == 1 && testActivity.getParentProject().getStartEndWeeks()[1] == 2);
+        assertTrue(testActivity.getParentProject().getStartEndWeeks()[0] == int1 && testActivity.getParentProject().getStartEndWeeks()[1] == int2);
     }
 
     @Given("an activity exists with start week {int} and end week {int}")
