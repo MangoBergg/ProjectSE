f26c540cb4f29ca755502ec0ea03127f80287e74
MangoBergg <s224763@student.dtu.dk>
2024-05-05 23:30:13 +0200

    Fix bug with deleting activity

--- src/main/java/system/program/strategies/DeleteProjectStrategy.java
+++ src/main/java/system/program/strategies/DeleteProjectStrategy.java
@@ -1,8 +1,11 @@
 package system.program.strategies;
 
+import java.util.List;
 import java.util.Scanner;
 
+import system.program.interfaces.IActivity;
 import system.program.interfaces.IEmployee;
+import system.program.interfaces.IProject;
 import system.program.model.ErrorMessageHolder;
 import system.program.model.ProjectManagementApp;
 
@@ -11,9 +14,27 @@ public class DeleteProjectStrategy implements UserActionStrategy {
     public void execute(Scanner inputScanner, ProjectManagementApp projectManagementApp, ErrorMessageHolder errorMessage, IEmployee user) {
         try {
             System.out.println("Which project do you want to delete?: ");
-            projectManagementApp.getProjectRepository().getProjectList().remove(Integer.parseInt(inputScanner.nextLine()) - 1);
+            int projectNumber = Integer.parseInt(inputScanner.nextLine()) - 1;
+    
+            // Retrieve project list and validate the project number
+            List<IProject> projectList = projectManagementApp.getProjectRepository().getProjectList();
+            if (projectNumber < 0 || projectNumber >= projectList.size()) {
+                throw new IndexOutOfBoundsException("Invalid project number.");
+            }
+    
+            IProject project = projectList.get(projectNumber);
+            List<IActivity> activityListGlobal = projectManagementApp.getActivityRepository().getActivityList();
+            List<IActivity> activityListProject = project.getActivityList();
+    
+            // Iterator to avoid ConcurrentModificationException...
+            activityListGlobal.removeIf(activityGlobal -> activityListProject.stream()
+                    .anyMatch(activityProject -> activityProject.getName().equals(activityGlobal.getName())));
+
+            activityListProject.clear();
+            projectList.remove(projectNumber);
         } catch (Exception e) {
             errorMessage.setErrorMessage(e.getMessage());
         }
     }
+    
 }
