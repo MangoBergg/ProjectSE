caea469e0fe1ff4cff9734c5923a74d63057b973
MangoBergg <s224763@student.dtu.dk>
2024-05-06 21:32:26 +0200

    Delete activity deletes consumed time

--- src/main/java/system/program/strategies/DeleteActivityStrategy.java
+++ src/main/java/system/program/strategies/DeleteActivityStrategy.java
@@ -1,9 +1,11 @@
 package system.program.strategies;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 import system.program.interfaces.IActivity;
 import system.program.interfaces.IEmployee;
+import system.program.model.ConsumedTime;
 import system.program.model.ErrorMessageHolder;
 import system.program.model.Printer;
 import system.program.model.ProjectManagementApp;
@@ -18,8 +20,23 @@ public class DeleteActivityStrategy implements UserActionStrategy {
             int activityInt = Integer.parseInt(inputScanner.nextLine()) - 1;
             IActivity activity = projectManagementApp.getActivityRepository().getActivityList().get(activityInt);
             activity.getParentProject().getActivityList().remove(activity);
+
+            for (IEmployee employee : projectManagementApp.getEmployeeRepository().getEmployeeList()) {
+                Iterator<ConsumedTime> iterator = employee.getConsumedTimes().iterator();
+            
+                while (iterator.hasNext()) {
+                    ConsumedTime consumedTime = iterator.next();
+            
+                    if (activity.getName().equals(consumedTime.activity.getName())) {
+                        iterator.remove();
+                    }
+                }
+            }
+             
+
             projectManagementApp.getActivityRepository().getActivityList().remove(activityInt);
             
+
         } catch (Exception e) {
             errorMessage.setErrorMessage(e.getMessage());
         }
